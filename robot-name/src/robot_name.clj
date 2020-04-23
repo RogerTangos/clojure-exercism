ec9857768f07bd89a1db2cf8553b7e9e6f3b3955
Albert Carter <albert.r.carter@gmail.com>
2020-04-23 13:50:38 +0200

    fix race condition in robot_name, where robot id's might colide during addition

--- robot-name/src/robot_name.clj
+++ robot-name/src/robot_name.clj
@@ -1,26 +1,29 @@
 (ns robot-name)
 
-(def robot-map (atom {}))
+(def robot-map (ref {}))
 
 (defn rand-robot-name []
   (let [A #(char (+ (rand 26) 65))
         D #(rand-int 9)]
     (apply str ((juxt A A D D D)))))
 
+(defn new-robot-index [source]
+  "look at the provided source and increment a key.
+   If no keys are present, start with 0"
+  (->> (keys source)
+       ((fnil identity '(-1)))
+       (apply max)
+       (inc)))
+
 (defn robot []
-  ; new-robot-index: increment of the max key in robot-map atom
-  ; swap the robot map with an updated version
-  ; return the robot index
-  (let [new-robot-index (->> (keys @robot-map)
-                             ((fnil identity '(-1)))
-                             (apply max)
-                             (inc))]
-    (swap! robot-map #(assoc % new-robot-index (rand-robot-name)))
-    new-robot-index))
+  (dosync
+   (let [new-index (new-robot-index @robot-map)]
+     (alter robot-map #(assoc % new-index (rand-robot-name)))
+     new-index)))
 
 (defn robot-name [r]
   (@robot-map r))
 
 (defn reset-name [r]
-  (swap! robot-map #(assoc % r (rand-robot-name)))
+  (dosync (alter robot-map #(assoc % r (rand-robot-name))))
   r)
