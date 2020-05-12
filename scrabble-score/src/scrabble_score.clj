bf024a8f19073b3f9b5f43496a839b88e5ad1ccb
Albert Carter <albert.r.carter@gmail.com>
2020-05-12 16:56:03 +0200

    finish scrabble_score

--- scrabble-score/src/scrabble_score.clj
+++ scrabble-score/src/scrabble_score.clj
@@ -1,9 +1,14 @@
 (ns scrabble-score)
 
-(defn score-letter [] ;; <- arglist goes here
-      ;; your code goes here
-)
+(defn score-letter [x]
+  (condp some [(clojure.string/upper-case x)]
+    #{"A" "E" "I" "O" "U" "L" "N" "R" "S" "T"} 1
+    #{"D" "G"} 2
+    #{"B" "C" "M" "P"} 3
+    #{"F" "H" "V" "W" "Y"} 4
+    #{"K"} 5
+    #{"J" "X"} 8
+    #{"Q" "Z"} 10))
 
-(defn score-word [] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn score-word [word]
+  (reduce + (map score-letter word)))
