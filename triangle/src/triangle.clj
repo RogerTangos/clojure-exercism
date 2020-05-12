7583243bcfe57188cf0d2f77966b68f1e8b2c4c6
Albert Carter <albert.r.carter@gmail.com>
2020-05-12 14:30:12 +0200

    add triangle

--- triangle/src/triangle.clj
+++ triangle/src/triangle.clj
@@ -1,17 +1,21 @@
 (ns triangle)
 
-(defn is-valid? [] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn is-valid? [sides]
+  (let [[a b c] (sort sides)]
+    (and
+     (> (+ a b) c)
+     (not (some #{0} [a b c])))))
 
-(defn equilateral? [] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn equilateral? [& sides]
+  (and (is-valid? sides) (= 1 (count (distinct sides)))))
 
-(defn isosceles? [] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn isosceles? [& sides]
+  (and
+   (is-valid? sides) (>= 2 (count (distinct sides)))))
 
-(defn scalene? [] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn scalene? [& sides]
+  (and
+   (is-valid? sides) (= 3 (count (distinct sides)))))
+
+(defn mytest [& foo]
+  foo)
